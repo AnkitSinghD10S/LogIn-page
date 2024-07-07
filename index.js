64220c1f71db479b0035cf67b4623a19d972126d
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-07-07 21:37:00 +0530

    updates

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const express= require("express");
+const express = require("express");
 const db = require("./router/db-config");
-const cookie = require("cookie-parser")
-require('dotenv').config();
+const cookie = require("cookie-parser");
+require("dotenv").config();
 const port = process.env.port || 5000;
 const app = new express();
 app.use(cookie());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-db.connect((err)=>{
-    if(err) throw err;
-    console.log("database is connected");
-})
+db.connect((err) => {
+  if (err) throw err;
+  console.log("database is connected");
+});
 
+app.use("/js", express.static(__dirname + "/public/js"));
+app.use("/css", express.static(__dirname + "/public/css"));
+app.use("/", require("./router/pages"));
 
-app.use("/js",express.static(__dirname+'/public/js'))
-app.use("/css",express.static(__dirname+'/public/css'))
-app.use("/",require('./router/pages'))
+app.use("/api", require("./controllers/auth"));
 
-app.use("/api",require("./controllers/auth"))
-
-app.listen(port,()=>{
-    console.log(`sever is runing on ${port}`);
-})
+app.listen(port, () => {
+  console.log(`sever is runing on ${port}`);
+});
--- router/pages.js
+++ router/pages.js
@@ -4,20 +4,20 @@ const router = express.Router();
 const logout = require("../controllers/logout");
 const loggedIn = require("../controllers/loggedIn");
 
-router.get("/LogIn-page/", (req, res) => {
+router.get("/", (req, res) => {
   res.sendFile("index.html", { root: "./" });
 });
 
-router.get("/LogIn-page/signup", (req, res) => {
-  res.sendFile("signup.html", { root: "./public/html" });
+router.get("/signup", (req, res) => {
+  res.sendFile("signup.html", { root: "./" });
 });
 
-router.get("/LogIn-page/login", (req, res) => {
-  res.sendFile("login.html", { root: "./public/html" });
+router.get("/login", (req, res) => {
+  res.sendFile("login.html", { root: "./" });
 });
 
 router.get("/forget",(req,res)=>{
-    res.sendFile("forget.html",{root:"./public/html"})
+    res.sendFile("forget.html",{root:"./"})
 })
 
 router.get("/logout",logout)
@@ -27,7 +27,7 @@ router.get('/home', loggedIn, (req, res) => {
       if (req.headers.accept && req.headers.accept.indexOf('application/json') !== -1) {
           res.json({ userName: req.user.name });
       } else {
-          res.sendFile('home.html',{root:'./public/html'});
+          res.sendFile('home.html',{root:'./'});
       }
   } else {
       console.log("error");
