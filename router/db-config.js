b32d35b62618264cc06da5e577d07efe05c4da85
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-07-24 23:24:25 +0530

    updates

--- router/db-config.js
+++ router/db-config.js
@@ -5,6 +5,5 @@ const db = sql.createConnection({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
-  port:process.env.DB_PORT
 });
 module.exports = db;
